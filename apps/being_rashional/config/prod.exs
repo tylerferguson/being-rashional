ee9b7fa222d011ab60b2559602fb865bd58bb2f3
Tyler Ferguson <tyler@nested.com>
2017-11-18 22:22:43 +0000

    Fix compile error in being_rashional/prod.exs

--- apps/being_rashional/config/prod.exs
+++ apps/being_rashional/config/prod.exs
@@ -5,5 +5,5 @@ config :being_rashional, BeingRashional.Repo,
   database: "being_rashional",
   username: System.get_env("POSTGRES_USER"),
   url: System.get_env("DATABASE_URL"),
-  pool_size: Strig.to_integer(System.get_env("POOL_SIZE") || "10"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
